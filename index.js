d3fd8af06536589797073e1c990201014a865bc1
sannahan <73829699+sannahan@users.noreply.github.com>
2021-11-22 13:48:36 +0200

    Create backend

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) { 
+  return JSON.stringify(req.body) 
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+
+let contacts = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }]
+
+app.get('/api/persons', (request, response) => {
+  response.json(contacts)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${contacts.length} people</p><p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const contact = contacts.find(contact => contact.id === id)
+
+  if (contact) {
+    response.json(contact)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  contacts = contacts.filter(contact => contact.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number cannot be empty'
+    })
+  }
+
+  const names = contacts.map(contact => contact.name)
+  if (names.includes(body.name)) {
+    return response.status(400).json({
+      error: 'Contact has already been added'
+    })
+  }
+
+  const contact = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  contacts = contacts.concat(contact)
+  response.json(contact)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
