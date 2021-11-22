000160129e9285c7bcc123bd537c15f6ec85f421
sannahan <73829699+sannahan@users.noreply.github.com>
2021-11-22 17:30:18 +0200

    Add frontend build to backend

---
+++ README.md
@@ -0,0 +1,5 @@
+# Osan 3 tehtävät
+
+## Heroku
+
+[Linkki Herokuun](https://afternoon-harbor-67261.herokuapp.com/api/persons)
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let contacts = [
   {
